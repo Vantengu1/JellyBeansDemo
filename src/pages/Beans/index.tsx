37cff509608c9a583b97e0fe5eaafcab362bde00
Dmitriy Yaryshev <krestovcode@gmail.com>
2024-12-14 00:59:19 +0300

    delete search component

--- src/pages/Beans/index.tsx
+++ src/pages/Beans/index.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import Cards from "../../components/Cards";
-import Search from "../../components/Search";
 import style from './style.module.css'
 
 
 const Beans = () => {
 
-    const [filterValue, setFilterValue] = useState('');
 
     return (
         <div className={style.beans_container}>
-            <Search setFilterValue={setFilterValue}/>
-            <Cards filterValue={filterValue}/>
+            <Cards />
         </div>
     );
 };
